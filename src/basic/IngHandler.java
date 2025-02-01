e590066128a99718474e73351215b353733957eb
katherineh04 <katherineh04@vt.edu>
2025-02-01 18:32:23 -0500

    Update IngHandler.java

--- src/basic/IngHandler.java
+++ src/basic/IngHandler.java
@@ -1,10 +1,32 @@
 package basic;
+
+import java.util.ArrayList;
+
 public class IngHandler
 {
-    //~ Fields ................................................................
+    private ArrayList<String> ingredients;
+    private FileReader reader;
 
-    //~ Constructors ..........................................................
-
-    //~Public  Methods ........................................................
+    public IngHandler()
+    {
+       ArrayList<String> storedIng = reader.readFile();
+       ingredients = new ArrayList<String>();
+       // add in stored ingredients from earlier
+       if (!storedIng.isEmpty()) {
+           ingredients.addAll(storedIng);
+       }
+    }
+    
+    public boolean removeIngredient(String ing)
+    {
+        reader.remove(ing);
+        return ingredients.remove(ing);
+    }
+    
+    public void addIngredient(String ing)
+    {
+        reader.add(ing);
+        ingredients.add(ing);
+    }
 
 }
